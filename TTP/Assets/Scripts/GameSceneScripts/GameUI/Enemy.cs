a5141f476ea04354f78645f4861843ad0b4b5d58
Mountfog <cheonjo20@gmail.com>
2024-09-04 21:23:20 +0900

    EnemyChaseImport
    EnemyChaseImport

--- TTP/Assets/Scripts/GameSceneScripts/GameUI/Enemy.cs
+++ TTP/Assets/Scripts/GameSceneScripts/GameUI/Enemy.cs
@@ -1,18 +1,136 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Enemy : Unit
 {
     public Animator m_animator = null;
-    public Collider2D _col = null;
+    public SpriteRenderer _sr = null;
+    public CapsuleCollider2D _col = null;
     public Rigidbody2D _rigidBody = null;
     public float forceMult = 5f;
+    public Vector2 pos1;
+    public Vector2 pos2;
+    public Transform player;
+    public float dist;
+    public float speed = 10f;
+    private Vector2 _frameVelocity = Vector2.zero;
+    [SerializeField] private bool _grounded = true;
+    public LayerMask mask;
+    [SerializeField] private GameObject currentOneWayPlatform;
+    public EnemyState curEnemyState = EnemyState.Stop; 
+
+    public enum EnemyState
+    {
+        Stop = 0,
+        Follow = 1,
+        Death = 2,
+    }
+
     private void Awake()
     {
-        Initialize(30,2,3);
+        Initialize(30, 2, 3);
     }
-    public override void Initialize(int khealth,int kspeed, int kdamage)
+    private void FixedUpdate()
+    {
+        CheckCollisions();
+        HandleMove();
+        HandleGravity();
+        ApplyMovement();
+    }
+    private void HandleMove()
+    {
+
+        if (curEnemyState == EnemyState.Stop)
+        {
+            if (Vector2.Distance(transform.position, player.position) <= dist)
+            {
+                curEnemyState = EnemyState.Follow;
+                m_animator.SetBool("isMove", true);
+            }
+            //Move();
+        }
+        else
+        {
+             _frameVelocity.x = ((transform.position.x - player.position.x < 0f) ? 1f : -1f) * speed;
+            _sr.flipX = ((transform.position.x - player.position.x < 0f));
+
+            if (Mathf.Abs(transform.position.x - player.position.x) <= 1.5f)
+            {
+                if (!_grounded) return;
+                if (transform.position.y - player.position.y <= 0f)
+                {
+                    _frameVelocity.y = 24f;
+                    _grounded = false;
+                }
+                else if (transform.position.y - player.position.y > 0f)
+                {
+                    if (currentOneWayPlatform == null) return;
+                    TilemapCollider2D tc = currentOneWayPlatform.GetComponent<TilemapCollider2D>();
+                    Physics2D.IgnoreCollision(_col, tc);
+                    _grounded = true;
+
+                }
+            }
+        }
+    }
+    private void ApplyMovement() => _rigidBody.velocity = _frameVelocity;
+    private void HandleGravity()
+    {
+        if (_grounded && _frameVelocity.y <= 0f)
+        {
+            _frameVelocity.y = -0.15f;
+        }
+        else
+        {
+            var inAirGravity = 70f;
+            _frameVelocity.y = Mathf.MoveTowards(_frameVelocity.y, -20f, inAirGravity * Time.fixedDeltaTime);
+        }
+    }
+    void Move()
+    {
+        Vector3 dir = new Vector3(Mathf.PingPong(Time.time * speed, pos2.x - pos1.x) + pos1.x, pos1.y, 0f);
+        _rigidBody.MovePosition(dir);
+    }
+    private void CheckCollisions()
+    {
+        Physics2D.queriesStartInColliders = false;
+
+        int layermask = (-1) - (1 << LayerMask.NameToLayer("Unit"));
+
+        // Ground and Ceiling
+        RaycastHit2D groundRayCast = Physics2D.CapsuleCast(_col.bounds.center, _col.size, _col.direction, 0, Vector2.down, 0.05f, mask);
+        RaycastHit2D ceilingRayCast = Physics2D.CapsuleCast(_col.bounds.center, _col.size, _col.direction, 0, Vector2.down, 0.05f, mask);
+
+        bool groundHit = groundRayCast;
+        bool ceilingHit = ceilingRayCast;
+        // Hit a Ceiling
+        if (ceilingHit)
+        {
+            if (ceilingRayCast.collider.CompareTag("OneWayPlatform"))
+                _frameVelocity.y = Mathf.Min(0, _frameVelocity.y);
+        }
+        // Landed on the Ground
+        if (!_grounded && groundHit)
+        {
+            if (Physics2D.GetIgnoreCollision(groundRayCast.collider, _col))
+            {
+            }
+            else
+            {
+                Physics2D.IgnoreCollision(groundRayCast.collider, _col, false);
+                _grounded = true;
+            }
+        }
+        // Left the Ground
+        else if (_grounded && !groundHit)
+        {
+            _grounded = false;
+        }
+        Physics2D.queriesStartInColliders = true;
+    }
+    public override void Initialize(int khealth, int kspeed, int kdamage)
     {
         base.Initialize(khealth, kspeed, kdamage);
     }
@@ -28,13 +146,28 @@ public class Enemy : Unit
         //_rigidBody.AddForce(forceDirection, ForceMode2D.Impulse);
         GetComponentInChildren<Animator>().SetTrigger("hittrig");
         base.OnHit(hitPoint, damage);
-        GameMgr.Inst.damageTextMgr.CreateDamageText(damage,this.transform,hitPoint);
+        GameMgr.Inst.damageTextMgr.CreateDamageText(damage, this.transform, hitPoint);
         Camera.main.transform.GetComponent<CameraShake>().ShakeCamera();
+
     }
     public override void OnDeath()
     {
         GetComponentInChildren<Animator>().SetTrigger("deathtrig");
         GameMgr.Inst.gameScene.gameUI.EnemyKilled(this);
         Destroy(gameObject, 1.2f);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("OneWayPlatform"))
+        {
+            currentOneWayPlatform = collision.gameObject;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("OneWayPlatform"))
+        {
+            currentOneWayPlatform = null;
+        }
     }
 }
