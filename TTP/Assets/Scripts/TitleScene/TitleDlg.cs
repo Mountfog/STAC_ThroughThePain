4a39503affdbb3aabd71c01754ecd1350c2ddc57
s0.m6ng <galaxys20plusaurared@gmail.com>
2024-09-23 10:16:13 +0900

    commit

---
+++ TTP/Assets/Scripts/TitleScene/TitleDlg.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleDlg : MonoBehaviour
+{
+    [SerializeField] Button m_btnStart = null;
+    private void Start()
+    {
+        m_btnStart.onClick.AddListener(OnClicked_Start);
+    }
+    
+    void OnClicked_Start()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
